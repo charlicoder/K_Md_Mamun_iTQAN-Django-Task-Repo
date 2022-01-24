baa3c6fcde07858b9df9db81c2cb560a2e135d94
K Md Mamunur Rashid <charlicoder@gmail.com>
2022-01-24 07:32:12 +0600

    updated requirements file

--- core/settings.py
+++ core/settings.py
@@ -121,7 +121,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-STATIC_ROOT = BASE_DIR / 'public/staticfiles'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 STATICFILES_DIRS = (BASE_DIR / 'core/static',)
 
